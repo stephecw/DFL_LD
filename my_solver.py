fa60a83ad7843de2e3c994cfca75a619027ff214
Stéphane Eilles-Chan Way <stephane.eillesc@gmail.com>
2025-04-17 14:19:01 -0400

    solveur LD + train

--- my_solver.py
+++ my_solver.py
@@ -12,23 +12,18 @@ def solve_main_problem(c, mu, weights, capacity):
     weights : Poids des items sur toute les dimensions
     capacity : Capacités des items sur toute les dimensions
     """
-    weights_0 = weights[0]
-
-    mu_sum = mu.sum(dim=1)-mu[0]
-
-    capacity_0 = capacity[0]
-
+    mu_sum = mu.sum(dim=1)
     profit = c + mu_sum
-    
     n_items = len(profit)
     
     # Création d’un modèle knapsack
-    model = knapsackModel(n=n_items, budget=capacity_0, weight=weights_0)
+    model = knapsackModel(n=n_items, budget=capacity[0], weight=weights[0])
 
     # Résolution
     x_opt = model.solve(profit.detach().cpu().numpy())  # pyEPO attend un numpy
 
     return torch.tensor(x_opt, dtype=torch.float32)
+
 
 def solve_dual_subproblem(mu_i, weights_i, capacity_i):
     """
@@ -41,6 +36,26 @@ def solve_dual_subproblem(mu_i, weights_i, capacity_i):
     model = knapsackModel(n=n_items, budget=capacity_i, weight=weights_i)
     x_opt = model.solve(profit.detach().cpu().numpy())
     return torch.tensor(x_opt, dtype=torch.float32)
+
+
+def get_imle_solver_with_mu(weights, capacities, mu_batch):
+    """
+    Crée un solveur batch-compatible pour i-MLE,
+    qui intègre solve_main_problem avec les mu donnés (non appris).
+    """
+    def solver(profit_batch):
+        # profit_batch : [batch_size, n_items]
+        # mu_batch : [batch_size, m-1, n_items]
+        batch = profit_batch.size(0)
+        sol = []
+        for i in range(batch):
+            c_i = profit_batch[i]
+            mu_i = mu_batch[i]
+            x_opt = solve_main_problem(c_i, mu_i, weights, capacities)
+            sol.append(x_opt)
+        return torch.stack(sol)
+    
+    return solver
 
 
 
