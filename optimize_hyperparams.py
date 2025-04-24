2b4ca72506d54a535fa2d538f832cafdc497afe7
Stéphane Eilles-Chan Way <stephane.eillesc@gmail.com>
2025-04-23 22:36:23 -0400

    optimisation hyperpara

--- optimize_hyperparams.py
+++ optimize_hyperparams.py
@@ -1,111 +1,153 @@
-import wandb
+import optuna
 import torch
 from torch import optim
 from torch.utils.data import DataLoader
 from data_import import ImportDataset
-from train_imle import train, CustomMLP
+from train_imle import train, train_LD, test_regret, CustomMLP
 
 # Device setup
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Sweep configuration
-def get_sweep_config():
-    return {
-        'method': 'bayes',
-        'metric': {
-            'name': 'regret',
-            'goal': 'minimize'
-        },
-        'parameters': {
-            # Hyperparameters to tune
-            'lr': {
-                'distribution': 'log_uniform',
-                'min': 1e-5,
-                'max': 1e-1
-            },
-            'IMLE_n_samples': {
-                'distribution': 'int',
-                'min': 1,
-                'max': 50
-            },
-            'IMLE_sigma': {
-                'distribution': 'log_uniform',
-                'min': 0.01,
-                'max': 10
-            },
-            'IMLE_lambd': {
-                'distribution': 'log_uniform',
-                'min': 0.1,
-                'max': 100
-            },
-            # Static problem parameters
-            'dim': {'value': 5},
-            'num_feat': {'value': 200},
-            'num_item': {'value': 30},
-            'num_data_train': {'value': 500},
-            'num_data_test': {'value': 100},
-            'hidden_layer': {'value': 100},
-            'batch_size': {'value': 32},
-            'epochs': {'value': 20},
-            'sched_step_size': {'value': 10},
-            'sched_gamma': {'value': 0.1},
-            'IMLE_two_sides': {'value': False},
-            'IMLE_processes': {'value': 1}
-        }
-    }
+# Static problem parameters
+DIM = 5
+NUM_FEAT = 200
+NUM_ITEM = 50
+NUM_DATA_TRAIN = 500
+NUM_DATA_TEST = 100
+HIDDEN_LAYER = 100
+BATCH_SIZE = 32
+EPOCHS_LD = 30
+EPOCHS_classic = 3
+SCHED_STEP_SIZE = 10
+SCHED_GAMMA = 0.1
+IMLE_TWO_SIDES = False
+IMLE_PROCESSES = 1
 
-# Training function for the sweep
-def train_model():
-    # Initialize a new W&B run
-    with wandb.init() as run:
-        config = run.config
+# Objective for classic i-MLE
+def objective_classic(trial):
+    # Suggest hyperparameters
+    lr = trial.suggest_loguniform('lr', 1e-5, 1e-1)
+    n_samples = trial.suggest_int('IMLE_n_samples', 1, 50)
+    sigma = trial.suggest_loguniform('IMLE_sigma', 0.01, 10)
+    lambd = trial.suggest_loguniform('IMLE_lambd', 0.1, 100)
 
-        # Load datasets
-        train_set = ImportDataset(
-            f"datasets/train_{config.dim}_{config.num_feat}_{config.num_item}_{config.num_data_train}.txt"
-        )
-        test_set = ImportDataset(
-            f"datasets/test_{config.dim}_{config.num_feat}_{config.num_item}_{config.num_data_test}.txt"
-        )
-        train_loader = train_set.get_dataloader(batch_size=config.batch_size, shuffle=True)
-        test_loader = test_set.get_dataloader(batch_size=config.batch_size, shuffle=False)
+    # Load data
+    train_set = ImportDataset(f"datasets/train_{DIM}_{NUM_FEAT}_{NUM_ITEM}_{NUM_DATA_TRAIN}.txt")
+    test_set = ImportDataset(f"datasets/test_{DIM}_{NUM_FEAT}_{NUM_ITEM}_{NUM_DATA_TEST}.txt")
+    train_loader = train_set.get_dataloader(batch_size=BATCH_SIZE, shuffle=True)
+    test_loader = test_set.get_dataloader(batch_size=BATCH_SIZE, shuffle=False)
 
-        # Problem parameters
-        weights = train_set.get_weights(tensor=True)
-        capacities = train_set.get_capacities(tensor=True)
+    # Problem parameters
+    weights = train_set.get_weights(tensor=True)
+    capacities = train_set.get_capacities(tensor=True)
 
-        # Model
-        model = CustomMLP([config.num_feat, config.hidden_layer, config.num_item]).to(device)
+    # Model
+    model = CustomMLP([NUM_FEAT, HIDDEN_LAYER, NUM_ITEM]).to(device)
 
-        # Optimizer & scheduler
-        optimizer = optim.Adam(model.parameters(), lr=config.lr)
-        scheduler = optim.lr_scheduler.StepLR(
-            optimizer,
-            step_size=config.sched_step_size,
-            gamma=config.sched_gamma
-        )
+    # Optimizer & scheduler
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    scheduler = None #optim.lr_scheduler.StepLR(optimizer, step_size=SCHED_STEP_SIZE, gamma=SCHED_GAMMA)
 
-        # Train with i-MLE
-        train(
-            model, run,
-            train_loader, test_loader,
-            optimizer, scheduler,
-            weights, capacities,
-            epochs=config.epochs,
-            IMLE_n_samples=config.IMLE_n_samples,
-            IMLE_sigma=config.IMLE_sigma,
-            IMLE_lambd=config.IMLE_lambd,
-            IMLE_two_sides=config.IMLE_two_sides,
-            IMLE_processes=config.IMLE_processes,
-            verbose=False
-        )
-
-# Entry point: launch sweep
-if __name__ == '__main__':
-    sweep_config = get_sweep_config()
-    sweep_id = wandb.sweep(
-        sweep_config,
-        project='DFL_hyperopt',
-        entity='hugoper-polytechnique-montr-al'
+    # Train
+    train(
+        model, None,
+        train_loader, test_loader,
+        optimizer, scheduler,
+        weights, capacities,
+        epochs=EPOCHS_classic,
+        IMLE_n_samples=n_samples,
+        IMLE_sigma=sigma,
+        IMLE_lambd=lambd,
+        IMLE_two_sides=IMLE_TWO_SIDES,
+        IMLE_processes=IMLE_PROCESSES,
+        verbose=False
     )
-    wandb.agent(sweep_id, function=train_model, count=20)
+
+    # Evaluate regret
+    regret = test_regret(model, test_loader, weights, capacities)
+    return regret
+
+# Objective for LD
+def objective_LD(trial):
+    # Suggest hyperparameters
+    lr = trial.suggest_loguniform('lr', 1e-5, 1e-1)
+    n_samples = trial.suggest_int('IMLE_n_samples', 1, 50)
+    sigma = trial.suggest_loguniform('IMLE_sigma', 0.01, 10)
+    lambd = trial.suggest_loguniform('IMLE_lambd', 0.1, 100)
+
+    # Load data
+    train_set = ImportDataset(f"datasets/train_{DIM}_{NUM_FEAT}_{NUM_ITEM}_{NUM_DATA_TRAIN}.txt")
+    test_set = ImportDataset(f"datasets/test_{DIM}_{NUM_FEAT}_{NUM_ITEM}_{NUM_DATA_TEST}.txt")
+    train_loader = train_set.get_dataloader(batch_size=BATCH_SIZE, shuffle=True)
+    test_loader = test_set.get_dataloader(batch_size=BATCH_SIZE, shuffle=False)
+
+    # Problem parameters
+    weights = train_set.get_weights(tensor=True)
+    capacities = train_set.get_capacities(tensor=True)
+
+    # Model
+    model = CustomMLP([NUM_FEAT, HIDDEN_LAYER, NUM_ITEM]).to(device)
+
+    # Optimizer & scheduler
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    scheduler = None # optim.lr_scheduler.StepLR(optimizer, step_size=SCHED_STEP_SIZE, gamma=SCHED_GAMMA)
+
+    # Train with LD
+    train_LD(
+        model, None,
+        train_loader, test_loader,
+        optimizer, scheduler,
+        weights, capacities,
+        epochs=EPOCHS_LD,
+        IMLE_n_samples=n_samples,
+        IMLE_sigma=sigma,
+        IMLE_lambd=lambd,
+        IMLE_two_sides=IMLE_TWO_SIDES,
+        IMLE_processes=IMLE_PROCESSES,
+        verbose=False
+    )
+
+    # Evaluate regret
+    regret = test_regret(model, test_loader, weights, capacities)
+    return regret
+
+
+def main():
+    # Study for classic i-MLE
+    study_classic = optuna.create_study(direction='minimize', study_name='classic_iMLE')
+    study_classic.optimize(objective_classic, n_trials=20)
+
+    # Study for LD
+    study_ld = optuna.create_study(direction='minimize', study_name='LD_iMLE')
+    study_ld.optimize(objective_LD, n_trials=20)
+
+    # Retrieve best hyperparameters
+    best_classic = study_classic.best_params
+    best_ld = study_ld.best_params
+
+    # Save to CSV
+    import csv
+    with open('best_hyperparams.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['study', 'lr', 'IMLE_n_samples', 'IMLE_sigma', 'IMLE_lambd','dim', 'num_feat', 'num_item', 'num_data_train'])
+        writer.writerow([
+            'classic',
+            best_classic['lr'],
+            best_classic['IMLE_n_samples'],
+            best_classic['IMLE_sigma'],
+            best_classic['IMLE_lambd'],
+            DIM, NUM_FEAT, NUM_ITEM, NUM_DATA_TRAIN
+        ])
+        writer.writerow([
+            'LD',
+            best_ld['lr'],
+            best_ld['IMLE_n_samples'],
+            best_ld['IMLE_sigma'],
+            best_ld['IMLE_lambd'],
+            DIM, NUM_FEAT, NUM_ITEM, NUM_DATA_TRAIN
+        ])
+
+    print("Best hyperparameters saved to best_hyperparams.csv")
+
+if __name__ == '__main__':
+    main()
