98f7b3582901b5fa06ea0df128f68f0d76e70a10
hgprct <hugoperk@gmail.com>
2025-04-30 12:21:55 -0400

    Update import_data pour portfolio

--- portfolio/data_import.py
+++ portfolio/data_import.py
@@ -17,23 +17,23 @@ class ImportDataset:
             self.z_std[self.z_std == 0] = 1.0         # évite div/0
         else:                             # on ré-utilise celles passées
             self.z_mean, self.z_std = z_stats
-        Z_tensor = (Z_tensor - self.z_mean) / self.z_std
+        Z_tensor = (Z_tensor - self.z_mean) / self.z_std # Normalisation
 
         self.Z_tensor  = Z_tensor
-        self.c_tensor  = torch.tensor(self.c , dtype=torch.float32)
-        self.x_tensor  = torch.tensor(self.x , dtype=torch.int32)
+        self.r_tensor  = torch.tensor(self.r , dtype=torch.float32)
+        self.x_tensor  = torch.tensor(self.x , dtype=torch.float32)
         self.X_tensor  = torch.tensor(self.X , dtype=torch.int32)
         self.mu_tensor = torch.tensor(self.mu, dtype=torch.float32)
 
 
         if model is not None:
             self.dataset = dataset.optDataset(
-                model, self.Z_tensor, self.c_tensor,
+                model, self.Z_tensor, self.r_tensor,
                 self.x_tensor, self.X_tensor, self.mu_tensor
             )
         else:
             self.dataset = TensorDataset(
-                self.Z_tensor, self.c_tensor,
+                self.Z_tensor, self.r_tensor,
                 self.x_tensor, self.X_tensor, self.mu_tensor
             )
     
@@ -43,32 +43,28 @@ class ImportDataset:
         """
         with open(fname, 'r') as f:
             lines = f.readlines()
-            self.dim, self.num_feat, self.num_item, self.num_data = map(int, lines[0].split(","))
-            self.capacities = []
-            self.weights = []
-            for i in range(self.dim):
-                line = lines[i+1].split(",")
-                self.capacities.append(int(line[0]))
-                self.weights.append(list(map(int, line[1:])))
-            self.capacities = np.array(self.capacities)
-            self.weights = np.array(self.weights)
+            self.num_data, self.num_feat, self.num_item, self.gamma = map((int, int, int, float), lines[0].split(","))
+            self.cov = []
+            for i in range(self.num_item):
+                self.cov.append(list(map(float, lines[i+1].split(","))))
+            self.cov = np.array(self.cov)
             self.Z = []
-            self.c = []
+            self.r = []
             self.x = []
             self.X = []
             self.mu = []
             for i in range(self.num_data):
                 line = lines[self.dim+1+i].split(",")
                 self.Z.append(list(map(float, line[:self.num_feat])))
-                self.c.append(list(map(int, line[self.num_feat:self.num_feat+self.num_item])))
-                self.x.append(list(map(int, line[self.num_feat+self.num_item:self.num_feat+self.num_item+self.num_item])))
-                self.X.append(list(map(int, line[self.num_feat+self.num_item+self.num_item:self.num_feat+self.num_item+self.num_item+self.num_item])))
-                self.mu.append(list(map(float, line[self.num_feat+self.num_item+self.num_item+self.num_item:])))
+                self.r.append(list(map(float, line[self.num_feat : self.num_feat + self.num_item])))
+                self.x.append(list(map(float, line[self.num_feat + self.num_item : self.num_feat + 2*self.num_item])))
+                self.X.append(list(map(int, line[self.num_feat + 2*self.num_item : self.num_feat + 3*self.num_item])))
+                self.mu.append(list(map(float, line[self.num_feat + 3*self.num_item :])))
             self.Z = np.array(self.Z)
-            self.c = np.array(self.c)
+            self.r = np.array(self.r)
             self.x = np.array(self.x)
             self.X = np.array(self.X)
-            self.mu = np.array(self.mu).reshape(self.num_data, self.dim-1, self.num_item)
+            self.mu = np.array(self.mu)
 
     def get_z_stats(self):
         """Retourne (mean, std) utilisés pour la normalisation."""
@@ -77,29 +73,26 @@ class ImportDataset:
     def get_sizes(self):
         """
         Retourne les tailles du dataset sous la forme (dim, num_feat, num_item, num_data).
-        dim : int : Nombre de dimensions.
+        num_data : int : Nombre de données.
         num_feat : int : Nombre de features.
         num_item : int : Nombre d'items.
-        num_data : int : Nombre de données.
         """
-        return self.dim, self.num_feat, self.num_item, self.num_data
-    def get_capacities(self, tensor=False):
+        return self.num_data, self.num_feat, self.num_item
+    
+    def get_gamma(self, tensor=False):
         """
-        Retourne les capacités du dataset.
-        tensor : bool : Si True, retourne un tenseur PyTorch.
+        Retourne le paramètre gamma du problème de portfolio.
         """
-        if tensor:
-            return torch.tensor(self.capacities, dtype=torch.int32)
-        return self.capacities
+        return self.gamma
         
-    def get_weights(self, tensor=False):
+    def get_cov(self, tensor=False):
         """
-        Retourne les poids du dataset.
+        Retourne la matrice de covariance du problème de portfolio.
         tensor : bool : Si True, retourne un tenseur PyTorch.
         """
         if tensor:
-            return torch.tensor(self.weights, dtype=torch.int32)
-        return self.weights
+            return torch.tensor(self.cov, dtype=torch.float32)
+        return self.cov
     
     def get_dataset(self):
         """
@@ -109,8 +102,8 @@ class ImportDataset:
     
     def get_dataloader(self, batch_size=32, shuffle=True):
         """
-        Retourne le DataLoader PyTorch, avec des batchs de (Z, c, X°, [mu_2, mu_3, ...]).
-        batch_size : int : Taille du batch.
+        Retourne le DataLoader PyTorch, avec des batchs de (Z, r, x, X°, mu).
+        batch_size : int : Taille des batchs.
         shuffle : bool : Si True, mélange les données.
         """
         dataloader = DataLoader(self.dataset, batch_size=batch_size, shuffle=shuffle)
