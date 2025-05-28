f0662ccb28e48e5674248c2dc0029488ced0bf2c
Stéphane Eilles-Chan Way <stephane.eillesc@gmail.com>
2025-05-28 14:57:11 -0400

    portfolio temps

--- job_test5.sh
+++ job_test5.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --time=03:00:00          # Temps d'exécution (1 heure)
+#SBATCH --time=01:20:00          # Temps d'exécution (1 heure)
 #SBATCH --account=def-qcappart   # Remplacez par votre compte
 #SBATCH --mem=4G                 # Mémoire requise (4 Go)
 #SBATCH --cpus-per-task=1        # Nombre de CPU
 #SBATCH --gres=gpu:1             # Si vous avez besoin d'un GPU
 #SBATCH --job-name=job_test5    # Nom du job
-#SBATCH --output=output.log     # Fichier de sortie
-#SBATCH --error=error.log
+#SBATCH --output=output5.log     # Fichier de sortie
+#SBATCH --error=error5.log
 
 module load StdEnv/2023
 module load cuda/12.2
@@ -14,4 +14,4 @@ module load python/3.10
 module load scipy-stack
 source ~/env_projet/bin/activate
 
-python -m knapsack.gen_data --n_train 0 --n_test 200 --n 50 --dim 10
+python -m portfolio.run_experiments --n 50 --ep_cla 0 --ep_sg 500 --ep_mse 0 --ep_ld 0 --method Exact
