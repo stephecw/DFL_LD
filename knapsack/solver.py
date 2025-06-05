22a6ca875cac2d79648c192e6e174522858577be
hgprct <hugoperk@gmail.com>
2025-06-05 10:10:23 -0400

    update opti X mu

--- knapsack/solver.py
+++ knapsack/solver.py
@@ -49,81 +49,12 @@ def dp_knapsack_gpu_batch(capacity, weights, c, num_items, X, Ldp):
         else:
             X[idx_batch * num_items + i - 1] = 0
 
-class solver_X_1D_knapsack_test():
-
-    def __init__(self, weights, capacity, device):
-        self.weights = torch.tensor(weights, dtype=torch.int32, device=device)
-        self.capacity = torch.tensor([capacity], dtype=torch.int32, device=device)
-        self.num_items = weights.shape[0]
-        self.violation = None
-        self.device = device
-    
-    def __call__(self, c):
-        batch_size = c.shape[0]
-        self.X = torch.zeros(batch_size*self.num_items, dtype=torch.int32, device=self.device)
-        c = c.clone().view(batch_size*self.num_items).to(self.device)
-        Ldp = torch.zeros((batch_size, self.capacity.item() + 1, self.num_items + 1), dtype=torch.int32, device=self.device)
-        
-        dp_knapsack_gpu_batch[batch_size, 1](self.capacity, 
-                                             self.weights, 
-                                             c, 
-                                             self.num_items, 
-                                             self.X, 
-                                             Ldp)
-        
-        self.violation = self.capacity - self.X * self.weights
-        return self.X.view(batch_size, self.num_items)
-    
-    def get_violation(self):
-        return self.violation
-        
-@cuda.jit
-def dp_knapsack_gpu_batch(capacity, weights, c, num_items, X, Ldp):
-    idx_batch = cuda.grid(1)
-    cap = capacity[0]
-    for i in range(1, num_items + 1):
-        for w in range(cap + 1):
-            if weights[i - 1] <= w:
-                Ldp[idx_batch][w][i] = max(Ldp[idx_batch][w][i - 1],
-                                            Ldp[idx_batch][w - weights[i - 1]][i - 1] + c[idx_batch * num_items + i - 1])
-            else:
-                Ldp[idx_batch][w][i] = Ldp[idx_batch][w][i - 1]
-
-    # Backtracking to find selected items
-    w = capacity[0]
-    for i in range(num_items, 0, -1):
-        if Ldp[idx_batch][w][i] != Ldp[idx_batch][w][i - 1]:
-            X[idx_batch * num_items + i - 1] = 1
-            w -= weights[i - 1]
-
-class solver_X_knapsack_CPU():
-
-    def __init__(self, weights, capacity):
-        self.model = knapsackModel(weights=weights, capacity=capacity)
-    
-    def __call__(self, c):
-        for i in range(c.shape[0]):
-            self.model.setObj(c[i])
-            x_i, _ = self.model.solve()
-            if i == 0:
-                X = x_i
-            else:
-                X = np.vstack((X, x_i))
-        return torch.tensor(X, dtype=torch.int32)
-
 class solver_X_knapsack():
     """
     Solver for the knapsack problem with multiple dimensions.
-
     """
-    def __init__(self, weights, capacities, device, n_jobs=-1):
-        self.weights = weights.detach().cpu().numpy() if torch.is_tensor(weights) else weights.copy()
-        self.capacities = capacities.detach().cpu().numpy() if torch.is_tensor(capacities) else capacities.copy()
-        self.num_items = weights.shape[0]
-        self.device = device
-        self.n_jobs = n_jobs
-        self.solver = knapsackModel(weights=self.weights, capacity=self.capacities)
-
+    def __init__(self, weights, capacities):
+        self.solver = knapsackModel(weights=weights, capacity=capacities)
 
     def __call__(self, c_batch):
         """
