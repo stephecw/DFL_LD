286265eee259a8424a543c0f46ac9555ffeb5247
hgprct <hugoperk@gmail.com>
2025-06-05 09:23:22 -0400

    base datasets

---
+++ knapsack/jobfiles/gen_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=03:00:00          # Temps d'exécution (1 heure)
+#SBATCH --account=def-qcappart   # Remplacez par votre compte
+#SBATCH --mem=4G                 # Mémoire requise (4 Go)
+#SBATCH --cpus-per-task=1        # Nombre de CPU
+#SBATCH --gres=gpu:1             # Si vous avez besoin d'un GPU
+#SBATCH --job-name=data    # Nom du job
+#SBATCH --output=knapsack/output/output.log     # Fichier de sortie
+#SBATCH --error=knapsack/output/error.log
+
+module load StdEnv/2023
+module load cuda/12.2
+module load python/3.10
+source ~/env_projet/bin/activate
+
+python -m knapsack.gen_data --n 50 100 200 --dim 5 10
